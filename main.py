1fb7a5e0a2480df584af42f6cfbea57b91d78545
Bartu Baş <bartubas99@gmail.com>
2022-12-20 22:43:52 +0300

    new way to create table

--- main.py
+++ main.py
@@ -2,32 +2,9 @@ import socket
 import sys
 
 
-def create_table(node_count):
-    table = { }
-
-    # Use a very high value instead of infinity
-    inf = 100_000
-
-    # Port numbers start from 3000
-    initial_node = 3000
-
-    for x in range(initial_node, initial_node + node_count):
-        for y in range(initial_node, initial_node + node_count):
-            write_table(x, y, table, inf)
-
-    return table
-
-
 def write_table(x, y, table, cost):
     table[(x, y)] = cost
     table[(y, x)] = cost
-    return
-
-
-def update_cost(x, y, table, cost):
-    current_cost = table[(x, y)]
-    if cost < current_cost:
-        write_table(x, y, table, cost)
     return
 
 
@@ -37,14 +14,14 @@ def parse_file(file_name, port):
 
     node_count = int(lines[0])
 
-    table = create_table(node_count)
+    table = { }
 
     for x in range(1, len(lines)):
         line = lines[x]
         sp = line.split(" ")
         other_port = int(sp[0])
         cost = int(sp[1])
-        update_cost(other_port, port, table, cost)
+        write_table(other_port, port, table, cost)
 
     return table
 
@@ -81,6 +58,7 @@ def program():
                 if not data:
                     break
 
+                print("I should be sending data now!")
                 # TODO: Send your table to others
                 connection.sendall(data)
 
